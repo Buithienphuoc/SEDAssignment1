e07560d2b0f9db6a9d93ef18f7b95d2a764c482e
Nhung <nhungmaitran1412@gmail.com>
2021-03-31 16:13:07 +0700

    viet function chay stoi

--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
-#include <string>
+#include <string.h>
 #include <fstream>
-#include <iomanip>
+#include <cmath>
+//#include <iomanip>
 
 using namespace std;
 
@@ -10,13 +11,63 @@ class Contents{
         int x, y;
 };
 
-// Function declarations:
-void showTeamInformation();
-void openFile(const string &filePath, ifstream& ip); //open file for reading only
-int findNumberOfLines(string x, ifstream& ip);
-void returnToBeginOfFile(ifstream& ip);
-void storeFileIntoArrays(Contents *array, string str, string str2, ifstream& ip);
-void outArray(Contents *array, int count_line);
+void showTeamInformation(){
+    cout << "ASSIGNMENT 1GROUP<TT>" << endl
+         << "s3634831,s3634831@rmit.edu.vn, Bui , Phuoc" << endl
+         << "s3695656,s3695656@rmit.edu.vn, Nguyen , Trung" << endl
+         << "s3879954,s3879954@rmit.edu.vn, Tran,Nhung" << endl
+         << "s3877981,s3877981@rmit.edu.vn, Ngo , Nguyet" << endl;
+}
+void openFile(const string& filePath, ifstream& ip) {
+    ip.open(filePath, ios::in);
+    if (!ip.is_open()) {
+        showTeamInformation();
+        cerr << "ERROR: File Open" << endl;
+    }
+}
+int findNumberOfLines(string x, ifstream& ip) {
+    int count = 0;
+    while (ip.good()) {
+        getline(ip, x, '\n');
+        count ++;
+    }
+    return count;
+}
+void returnToBeginOfFile(ifstream& ip) {
+    ip.clear();
+    ip.seekg(1);
+}
+int strToInt(string str) {
+    int index = 0, num = 0, dec = pow(10, str.size())/10;
+    while (str[index] != '\0') {
+        num += ((str[index] - '0') * dec);
+        dec /= 10;
+        index ++;
+    }
+    return num;
+}
+void storeFileIntoArrays(Contents *array, string str, string str2, ifstream& ip) {
+    int index = 0; //run index 1++, b/c index 0 is 'x','y'
+    while (ip.good()) {
+        getline(ip, str, ',');
+        array[index].x = strToInt(str);
+        cout <<"num: " <<array[index].x <<endl;
+        getline(ip, str2, '\n');
+        array[index].y = strToInt(str2);
+        index ++;
+    }
+}
+void outArray(Contents *array, int count_line) {
+    cout << "The array: \n";
+    for (int index = 0; index < count_line; index ++) {
+        cout << setw(4) << array[index].x
+             << setw(4) << "||"
+             << setw(4) << array[index].y << endl;
+    }
+}
+void doExercise1(){
+    // add your code here
+}
 
 // Main function:
 int main() {
@@ -49,54 +100,3 @@ int main() {
 }
 
 // Function detail:
-void showTeamInformation(){
-    cout << "ASSIGNMENT 1GROUP<TT>" << endl
-    << "s3634831,s3634831@rmit.edu.vn, Bui , Phuoc" << endl
-    << "s3695656,s3695656@rmit.edu.vn, Nguyen , Trung" << endl
-    << "s3879954,s3879954@rmit.edu.vn, Tran,Nhung" << endl
-    << "s3877981,s3877981@rmit.edu.vn, Ngo , Nguyet" << endl;
-}
-void openFile(const string& filePath, ifstream& ip) {
-    ip.open(filePath, ios::in);
-    if (!ip.is_open()) {
-        showTeamInformation();
-        cerr << "ERROR: File Open" << endl;
-    }
-}
-int findNumberOfLines(string x, ifstream& ip) {
-    int count = 0;
-    while (ip.good()) {
-        getline(ip, x, '\n');
-        count ++;
-    }
-    return count;
-}
-void returnToBeginOfFile(ifstream& ip) {
-    ip.clear();
-    ip.seekg(1);
-}
-void storeFileIntoArrays(Contents *array, string str, string str2, ifstream& ip) {
-    int index = 0;
-    string::size_type sz;
-    while (ip.good()) {
-        getline(ip, str, ',');
-        cout << "str: " << str<< endl;
-        cout << str << endl;
-        // array[index].x = stoi(str,&sz);
-        cout << str2 << endl;
-        getline(ip, str2, '\n');
-        // array[index].y = stoi(str2,&sz);
-        index ++;
-    }
-}
-void outArray(Contents *array, int count_line) {
-    cout << "The array: \n";
-    for (int index = 0; index < count_line; index ++) {
-        cout << setw(4) << array[index].x
-            << setw(4) << "||"
-            << setw(4) << array[index].y << endl;
-    }
-}
-void doExercise1(){
-    // add your code here
-}
