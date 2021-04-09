58511488105162fb37604c9ea2fca7e454c74471
Nhung <nhungmaitran1412@gmail.com>
2021-04-09 22:12:32 +0700

    sua linked list

--- main.cpp
+++ main.cpp
@@ -3,8 +3,17 @@
 #include <fstream>
 #include <cmath>
 #include <iomanip>
-
+//acknowledment: https://www.codespeedy.com/how-to-insert-a-node-in-linked-list-in-cpp/#:~:text=Adding%20a%20node%20at%20the%20front.%201%20First,node%2A%20newnode%20%3D%20%28struct%20node%2A%29%20malloc%28sizeof%28struct%20...%20
 using namespace std;
+
+typedef struct {
+    int x, y;
+} Contents;
+struct Node{
+    Contents data;
+    Node *next;
+};
+typedef struct Node *nodePtr;
 
 void showTeamInformation(){
     cout << "ASSIGNMENT 1GROUP<TT>" << endl
@@ -13,25 +22,14 @@ void showTeamInformation(){
          << "s3879954,s3879954@rmit.edu.vn, Tran,Nhung" << endl
          << "s3877981,s3877981@rmit.edu.vn, Ngo , Nguyet" << endl;
 }
+
 void openFile(const string& filePath, ifstream& ip) {
     ip.open(filePath, ios::in);
     if (!ip.is_open()) {
-        showTeamInformation();
         cerr << "ERROR: File Open" << endl;
     }
 }
-int findNumberOfLines(string x, ifstream& ip) {
-    int count = 0;
-    while (ip.good()) {
-        getline(ip, x, '\n');
-        count ++;
-    }
-    return count;
-}
-void returnToBeginOfFile(ifstream& ip) {
-    ip.clear();
-    ip.seekg(1);
-}
+
 int strToInt(string str) {
     int index = 0, num = 0, dec = pow(10, str.size())/10;
     while (str[index] != '\0') {
@@ -41,44 +39,109 @@ int strToInt(string str) {
     }
     return num;
 }
-void storeFileIntoArrays(int *array_x, int *array_y, string str, ifstream& ip) {
-    int index = 0; //run index 1++, b/c index 0 is 'x','y'
-    while (ip.good()) {
-        getline(ip, str, ',');
-        array_x[index] = strToInt(str);
-        getline(ip, str, '\n');
-        array_y[index] = strToInt(str);
-        index ++;
+void init(nodePtr &pHead) {
+    pHead = NULL;
+}
+nodePtr createNode(Contents d) {
+    nodePtr p = new Node;
+    if (p != NULL) {
+        p->data = d;
+        p->next = NULL;
     }
+    return p;
+}
+void addLastNode(nodePtr &pHead, Contents value) {
+    nodePtr newNode = new Node;
+    nodePtr lastNode = pHead;
+    newNode->data = value;
+    newNode->next = NULL;
+    if (pHead == NULL) {
+        pHead = newNode;
+        return;
+    }
+    while (lastNode->next != NULL)
+        lastNode = lastNode->next;
+    lastNode->next = newNode;
+}
+int createList(nodePtr &pHead, ifstream &ip, string str) {
+    //create list to store file's contents into the list
+    Contents d;
+    int countLine = 0;
+    getline(ip, str); //skip the first line (x,y)
+    while (!ip.eof()) {
+        getline(ip, str, ',');
+        d.x = strToInt(str);
+        getline(ip, str, '\n');
+        d.y = strToInt(str);
+        nodePtr p = createNode(d);
+        if (p != NULL) {
+            addLastNode(pHead, d);
+            countLine ++;
+        }
+    }
+    return countLine;
+}
+void showList(nodePtr pHead)
+{
+    for (nodePtr k = pHead; k != NULL;k = k->next) {
+        cout << k->data.x << " " << k->data.y << endl;
+    }
+}
+
+bool checkArguments(int argc) {
+    //check number of arguments
+    if (argc != 2) {
+        cerr << "Invalid number of arguments.";
+        return false;
+    }
+    return true;
 }
 
 ////Math functions:
 
-double mean(int *array, int countLine) {
-    int sum = 0;
-    for (int i = 1; i < countLine; i ++) {
-        sum += array[i];
+Contents mean(nodePtr &pHead, int countLine) {
+    Contents m;
+    m.x = 0;
+    m.y = 0;
+    for (nodePtr temp = pHead; temp != nullptr; temp ++) {
+        m.x += temp->data.x;
+        m.y += temp->data.y;
     }
-    return (sum / countLine);
+    m.x /= countLine;
+    m.y /= countLine;
+    return m;
 }
 
-double median(int *array, int countLine) {
-
-    if (countLine % 2 != 0) {
-        return array[countLine / 2];
-    } else {
-        return (double)(array[(countLine - 1) / 2] + array[countLine / 2]) / 2;
+Contents median(nodePtr &pHead, int countLine) {
+    nodePtr temp = pHead;
+    int index = 0;
+    while (temp != NULL) {
+        index ++;
+        if (countLine % 2 != 0 && index == (countLine % 2)) {
+            return temp->data;
+        }
+        else if (countLine % 2 == 0 && index == (countLine % 2)){
+            return temp->data;
+        }
     }
+}
+////Function to calculate Mode
+Contents mode(nodePtr &pHead, int countLine) {
+    Contents m = mean(pHead, countLine), med = median(pHead, countLine), mod;
+    mod.x = m.x - 3 *(m.x -med.x);
+    mod.y = m.y - 3 *(m.y -med.y);
+    return mod;
 }
 ////Function to calculate Variance
-double variance(int* array, int countLine) {
+Contents variance(nodePtr &pHead, int countLine) {
     // Compute sum squared
     // differences with mean.
-    double squaredDiff = 0, m = mean(array, countLine);
-    for (int i = 1; i < countLine; i++) {
-        squaredDiff += (array[i] - m) * (array[i] - m);
-    }
-    return (squaredDiff / countLine);
+    Contents squaredDiff, m = mean(pHead, countLine);
+    for (nodePtr temp = pHead; )
+//    for (int i = 1; i < countLine; i++) {
+//        squaredDiff += (array[i] - m) * (array[i] - m);
+//    }
+//    return (squaredDiff / countLine);
 }
 
 ////Function to calculate Deviation
@@ -98,7 +161,11 @@ double meanAbsoluteDeviation(int* array, int countLine) {
 }
 
 ////Function to calculate First Quartile
-
+double firstQuartile(int*array, int countLine){
+    double med  = median(array, countLine);
+    double med1 = median(array, med);
+    return med1;
+}
 
 //// Function to calculate skewness.
 double skewness(int* array, int countLine){
@@ -158,12 +225,20 @@ void linearRegression(int *array_x, int *array_y, int countLine) {
     //output the equation
     cout << "The linear equation: y = " << slope << "x + " << intercept << endl;
 }
-void printDouble(double num) {
-
+void printDouble(string str, double result1, double result2) {
+    cout << str << " x = " << result1 << " - " << str << " y = " << result2 << endl;
 }
 // Main function:
-int main() {
+int main(int argc, char *argv[]) {
     ifstream ip;
+    nodePtr pHead;
+
+    //check if the arguments are valid
+    if (!checkArguments(argc)) {
+        showTeamInformation();
+        exit(1);
+    }
+
     //declare variables
     string filePath, str, str2;
     int countLine, *array_x, *array_y;
@@ -174,22 +249,16 @@ int main() {
 
     //open the file for reading
     openFile(filePath, ip);
-
-    //start inputting the program contents of csv file
-    countLine = findNumberOfLines(str, ip);
-    returnToBeginOfFile(ip);
-    // create array to store x and y with dynamic memory allocation
-    array_x = new int[countLine];
-    array_y = new int[countLine];
-    storeFileIntoArrays(array_x, array_y, str, ip);
-
-    //output array
-    // outArray(array, count_line);
-
+    init(pHead);
+    countLine = createList(pHead,ip, str);
     ip.close();
+
+    showList(pHead);
+
+
+    //output the results
+    cout << "median x = " <<
+
     showTeamInformation();
-    int m_x = mean(array_x, countLine), m_y = mean(array_y, countLine);
     return 0;
 }
-
-// Function detail:
